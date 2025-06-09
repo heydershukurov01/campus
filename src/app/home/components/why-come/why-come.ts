12a483774399dd55c9a4897853db6a01f19a5b76
Heyder Shukurov <heydar.shukurov@gmail.com>
2025-06-09 07:14:22 +0400

    Add home page sections

---
+++ src/app/home/components/why-come/why-come.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-why-come',
+  standalone: false,
+  templateUrl: './why-come.html',
+  styleUrl: './why-come.scss'
+})
+export class WhyCome {}
--- src/app/home/home-module.ts
+++ src/app/home/home-module.ts
@@ -6,18 +6,27 @@ import {Home} from './components/home/home';
 import {Header} from '../shared/layout/header/header';
 import {Hero} from '../shared/layout/hero/hero';
 import {HeroVideo} from '../shared/layout/hero-video/hero-video';
+import {Intro} from './components/intro/intro';
+import {Programmes} from './components/programmes/programmes';
+import {WhyCome} from './components/why-come/why-come';
 
 
 @NgModule({
   declarations: [
     Home,
+    Intro,
+    Programmes,
+    WhyCome,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     Header,
     Hero,
-    HeroVideo
+    HeroVideo,
+    Intro,
+    Programmes,
+    WhyCome
   ]
 })
 export class HomeModule { }
