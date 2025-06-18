1dbc2a6547ceee7c0204f0c318bc2d37d63e5474
Heydar Shukurov <heydarshukurov@Noutbuk-Fatima-2.local>
2025-06-18 15:37:19 +0400

    Contact Detail

--- dist/Campus/server/angular-app-manifest.mjs
+++ dist/Campus/server/angular-app-manifest.mjs
@@ -1,7 +1,7 @@
 
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
-  inlineCriticalCss: false,
+  inlineCriticalCss: true,
   baseHref: '/',
   locale: undefined,
   routes: [
@@ -12,8 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 731, hash: '321fcd47b11367996bd2a8137a1d09666c7d91b1ab3fe9c6d5e843be2c0549e9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1271, hash: '91d3a2bab29b363d500b108f46f9770e1057b30b5e4655f837bc8ec19ec32ded', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 34379, hash: '90251e7faa3a18a54021b676dc2cc9e69b1317757e7387eb7eeb104cfb00675f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)}
+    'index.csr.html': {size: 6348, hash: 'cc5eecbd37c15f6f63090822c4a172df8b3aebeb91ba4ea90eda43679e600acd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1054, hash: '8a2ad755b8a41bf29d949d43e0f52ea0f68c50365002aeb4288137614b5fd628', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 42442, hash: 'eac66d1f4bf9f0262241605935a447a2d8a97180261fbbb408ecc1dbf3c79b7a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-AGWN3U7U.css': {size: 94203, hash: 'BccnkOW4GfM', text: () => import('./assets-chunks/styles-AGWN3U7U_css.mjs').then(m => m.default)}
   },
 };
