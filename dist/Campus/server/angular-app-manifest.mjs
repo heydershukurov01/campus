ab5c1138a7cbad277ce28fd68bdc6afe9e8efaf4
Heydar Shukurov <heydarshukurov@Noutbuk-Fatima-2.local>
2025-07-02 11:22:26 +0400

    Language SUpport

--- dist/Campus/server/angular-app-manifest.mjs
+++ dist/Campus/server/angular-app-manifest.mjs
@@ -8,13 +8,22 @@ export default {
   {
     "renderMode": 2,
     "route": "/"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-BUG6VQA2.js",
+      "chunk-XJXSMSHQ.js"
+    ],
+    "route": "/what-is-campus-experience"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6348, hash: '2b7664a0e20d71578f31c21c7fac1bf8285ebb42cd1e318331c2e513b95bdb94', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1054, hash: 'c5c93db3ce45200291820c325b8e448028046c5a8957c4f31eec092ff0c0dd71', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 60898, hash: '443447989963af9c4f88753500ce8180641b833b1fcda570558df39cd2893899', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6348, hash: '8a0f2b746149cc52815925e86998d051a3d8f19e50ff0e3bbabbf54974f33130', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1054, hash: 'f57d1430f1dc5ce7a6cfe88de07f1bb6ea066faaf73d4ef97fcae8fded23f671', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'what-is-campus-experience/index.html': {size: 40915, hash: '28e58e1c72ea32b6b3bd16cbff26f97466c8a8e7874a3ff09ada203cf20c7b3d', text: () => import('./assets-chunks/what-is-campus-experience_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 64886, hash: 'bc1564c3bb50de7c1f39a859f0a70ef84ba0a213b9f16a732fb50e75a40147e8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-OAS3YAAG.css': {size: 100615, hash: '82xFkrf0CBo', text: () => import('./assets-chunks/styles-OAS3YAAG_css.mjs').then(m => m.default)}
   },
 };
--- src/app/shared/layout/hero/hero.ts
+++ src/app/shared/layout/hero/hero.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
+declare var google: any;
 @Component({
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.html',
   styleUrls: ['./hero.scss', './hero.responsive.scss']
 })
-export class Hero {
+export class Hero implements OnInit {
   isMenuOpen = false;
   submenuOpen = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+
+      // @ts-ignore
+      window.googleTranslateElementInit = () => {
+        // @ts-ignore
+        new window.google.translate.TranslateElement({
+          pageLanguage: 'en',
+          includedLanguages: 'az,en,es,ru',
+          // @ts-ignore
+          layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+      };
+
+      // Google Translate scriptini yükləyirik
+      const script = document.createElement('script');
+      script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+      document.body.appendChild(script);
+    }
+  }
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
 
